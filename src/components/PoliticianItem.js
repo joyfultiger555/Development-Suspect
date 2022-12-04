c7af1aac27eb3ffd0f9fbcf3b426f501113cf3ba
joyfultiger555 <114970320+joyfultiger555@users.noreply.github.com>
2022-12-03 20:18:08 -0500

    Update PoliticianItem.js

--- src/components/PoliticianItem.js
+++ src/components/PoliticianItem.js
@@ -1,4 +1,4 @@
-import '/Users/noahatanda/Desktop/uiux/Development-Anonymous/src/App.css';
+import '../App.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup  from 'react-bootstrap/ListGroup';
 
